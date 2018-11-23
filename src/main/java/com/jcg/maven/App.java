4b28225ef71e38a78c2d277fe8f14e25751d9c1b
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-23 17:01:49 +0100

    add issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -9,21 +9,6 @@ public class App
 
 
 
-
-
-
-    public class CarGitChecks{
-
-        public DriveTrain drive;
-
-        public void tearDown(){
-            System.out.println("Tear Down");
-        }
-
-        public void drive() {System.out.println("Drive");}  // Noncompliant; duplicates field name
-    }
-
-
     public void aMethod(){
     int i=0;
     while (i<10){
