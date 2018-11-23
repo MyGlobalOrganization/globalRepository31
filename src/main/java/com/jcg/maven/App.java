76b84d899768ec7e685f71c7e5ccfbf3df8e4181
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-23 16:50:55 +0100

    add issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,41 +8,12 @@ public class App
 {
 
 
-    private static final int MAX_PRICE = 10;
-
-    public static void main(String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
-
-
-
-
-    public void setPriceGitChecks(int price) {
-        assert price >= 0 && price <= MAX_PRICE;
-        // Set the price
-    }
-
-
-    public class CarGitChecks{
-
-        public DriveTrain drive;
-
-        public void tearDown(){
-            System.out.println("Tear Down");
-        }
-
-        public void drive() {System.out.println("Drive");}  // Noncompliant; duplicates field name
-    }
-
-
     public void aMethod(){
     int i=0;
     while (i<10){
         i++;
     }
-    return i;
+
     }
 
 
