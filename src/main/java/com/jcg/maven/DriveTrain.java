aa56515416ce76ffda46bcf4cfb8122389b4a77e
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-23 15:20:12 +0100

    removing issues

--- src/main/java/com/jcg/maven/DriveTrain.java
+++ src/main/java/com/jcg/maven/DriveTrain.java
@@ -1,4 +1,12 @@
 package com.jcg.maven;
 
 public class DriveTrain {
+    public int aMethod()
+    {
+        int i=0;
+        while (i<10){
+            i++;
+        }
+        return i;
+    }
 }
