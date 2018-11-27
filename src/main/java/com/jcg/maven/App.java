e408f53790be4164599c2ea8278171c49caf6d1b
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-27 14:13:22 +0100

    add issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -23,9 +23,14 @@ public class App
         int target = -5;
         int num = 3;
 
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
 
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
 
-
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
 
 
         String str = "/File|Name.txt";
