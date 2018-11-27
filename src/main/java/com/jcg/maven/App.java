f59c3e9918bbac8d5fd4054147a9fd705e776a6d
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-11-27 14:10:03 +0100

    add issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -23,8 +23,6 @@ public class App
         int target = -5;
         int num = 3;
 
-        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-        target =+ num; // Noncompliant; target = 3
 
 
 
