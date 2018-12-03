a33f62ddf5a3dcdf1fd025f484a50412cac01b0f
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-12-03 14:55:14 +0100

    add issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -7,7 +7,6 @@ package com.jcg.maven;
 public class App 
 {
 
-
     private static final int MAX_PRICE = 10;
 
     public static void main(String[] args )
@@ -20,6 +19,15 @@ public class App
     public void newMethodGitchecks(){
         int target = -5;
         int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
+
+
+        String str = "/File|Name.txt";
+
+        String clean = str.replaceAll(".",""); // Noncompliant; probably meant to remove only dot chars, but returns an empty string
+        String clean2 = str.replaceAll("|","_"); // Noncompliant; yields _/_F_i_l_e_|_N_a_m_e_._t_x_t_
 
 
     }
@@ -38,16 +46,15 @@ public class App
             System.out.println("Tear Down");
         }
 
-
+        public void drive() {System.out.println("Drive");}  // Noncompliant; duplicates field name
     }
 
 
-    public void aMethod() {
-        int i = 0;
-        while (i < 10) {
+    public void aMethod(){
+        int i=0;
+        while (i<10){
             i++;
         }
-    }
 
 
 
